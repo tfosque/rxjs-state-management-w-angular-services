bfc40c1dcfbcbf7917efcd6b494529ee015f356b
Tim Fosque <tim.fosque@becn.com>
2023-05-18 14:36:15 -0500

    wip

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -13,6 +13,10 @@ export class HomeComponent implements OnInit {
   colors: any = [];
   /*  */
 
+  objectMFG: any = {};
+  objectColor: any = {};
+
+  /*  */
   mfgSelLabel: any = '';
   colSelLabel: any = '';
   /*  */
@@ -35,39 +39,51 @@ export class HomeComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
-    const objectMFG = VariationMfg; // { ABC: ["265348", "22718", "282705"] }
-    const objectColor = VariationColors; // { Adobe(301): ["22720"] }
+    this.objectMFG = VariationMfg; // { ABC: ["265348", "22718", "282705"] }
+    this.objectColor = VariationColors; // { Adobe(301): ["22720"] }
+    const objectMFG = this.objectMFG;
+    const objectColor = this.objectColor;
     console.log( { objectMFG } ); console.log( { objectColor } );
 
+
+    this.findColor();
+  }
+
+  findColor() {
+    const objectMFG = this.objectMFG;
+    const objectColor = this.objectColor;
+
     /*  _zip, unzip, pullAt, without, xor, compact _.includes */
-    /*  const match = _.includes( ['40779', '568393'], '568393' );
-     console.log( { match } ); */
 
-    // console.log( 'objectColor[Adobe(301)]:', objectColor['Adobe(301)'].shift() );
-
-
+    // pass in each color
     const entries = Object.entries( objectMFG ).map( ( m: any ) => {
       // console.log( 'm1', m[1], 'objColor:', cp )// objectColor['']
 
-      const cp: any = objectColor['Acadia(750)'];
+      /* TODO dynamically change color */
+      const itemColor: any = objectColor['Adobe(301)'];
       // console.log( 'm[0]', m[0] )
 
-      if ( _.includes( m[1], cp[0] ) ) {
+      if ( _.includes( m[1], itemColor[0] ) ) {
         // console.log( { cp }, cp[0], m[0] );
         // console.log( 'objColor(m[0])' );
       }
-      return _.includes( m[1], cp[0] ) ? m : null;
+      return _.includes( m[1], itemColor[0] ) ? m : null;
     } )
     console.log( { entries } );
 
     const compact = _.compact( entries );
     console.log( { compact } );
+
+    /* disable items */
+    /* this.mfgs.map( ( m: any, index: number ) => {
+      _.includes( m[1], cp[0] 
+    } ) */
     this.mfgs = compact;
 
     console.log( 'this.options:', this.mfgs );
 
-    /*  */
-    this.colors = _.sortBy( Object.entries( objectColor ), [0] );
+    /* IMPROVE SORT */
+    this.colors = _.orderBy( Object.entries( objectColor ), item => item[0] );
     const sort = this.colors.sort( ( a: any, b: any ) => {
       return b[1] - a[1];
     } );
@@ -96,8 +112,14 @@ export class HomeComponent implements OnInit {
     this.getMatches();
   }
 
-  checkMatch( item: any ) {
+  highlightMatch( item: any ) {
     return _.includes( this.matches, item );
+  }
+
+  disableDropdownItem( item: any ) {
+    console.log( { item } )
+    // item[0]
+    return item;
   }
   getMatches() {
     const matches: any = _.intersection( this.skuDisplay, this.skuDisplayColor );
